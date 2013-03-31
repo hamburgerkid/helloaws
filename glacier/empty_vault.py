2c0561d62c6fb32d64003379f4d48f7e4652baa6
hamburgerkid <hamburgerkid@gmail.com>
2013-03-31 10:16:36 +0900

    Add Apache license text.

--- glacier/empty_vault.py
+++ glacier/empty_vault.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 # coding: UTF-8
+
+'''
+Copyright 2013 hamburgerkid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
 import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages/')
 import boto, datetime, getopt, json, time, traceback
@@ -89,7 +106,7 @@ job_output_file.close();
 
 log('Delete all archives in Glacier vault.');
 for idx, archive_info in enumerate(job_output['ArchiveList']):
-	if idx % 100 == 0:
+	if idx % 100 == 0: # per 100 id.
 		log('Renew Glacier object to avoid socket error while archive deletion.');
 		glacier_layer1 = Layer1(aws_access_key_id = aws_access_key_id, aws_secret_access_key = aws_secret_access_key, region_name = aws_region_name);
 	try:
